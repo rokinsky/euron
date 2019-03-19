da236514f54e29c917cc93beedbfc9551b3fb212
rokinsky <ankezy@gmail.com>
2019-03-19 12:51:41 +0100

    consts

--- src/euron.asm
+++ src/euron.asm
@@ -1,4 +1,5 @@
 global euron
+extern get_value, put_value
 
 section .rodata
     _SUM    db '+' ; 43
@@ -7,13 +8,13 @@ section .rodata
     _ZERO   db '0' ; 48
     _NINE   db '9' ; 57
     _NUM    db 'n' ; 110
-    _BIT    db 'B' ; 66
-    _POP    db 'C' ; 67
+    _BRCH   db 'B' ; 66
+    _CLN    db 'C' ; 67
     _DUP    db 'D' ; 68
     _EXCH   db 'E' ; 69
     _GET    db 'G' ; 71
     _PUT    db 'P' ; 80
-    _SLEEP  db 'S' ; 83
+    _SNCH   db 'S' ; 83
 
     SYS_WRITE equ 1
     SYS_EXIT  equ 60
@@ -22,9 +23,14 @@ section .rodata
 section .text
 
 euron:          ; "n(rdi), prog(rsi)"
+    push rbp
+    mov rbp, rsp
     mov rdx, rdi
     mov rdi, rsi
     call len
+
+    mov rsp, rbp
+    pop rbp
     ret
 
 len:          ; "prog(rdi)"
