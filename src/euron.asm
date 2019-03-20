5acdb603580a16dec8aa3abd6f6a82f8d744974d
rokinsky <ankezy@gmail.com>
2019-03-20 01:28:06 +0100

    refactoring

--- src/euron.asm
+++ src/euron.asm
@@ -1,144 +1,154 @@
 section .rodata
-    _SUM    equ '+' ; 43
-    _MUL    equ '*' ; 42
-    _NEG    equ '-' ; 61
-    _ZERO   equ '0' ; 48
-    _NINE   equ '9' ; 57
-    _NUM    equ 'n' ; 110
-    _BRCH   equ 'B' ; 66
-    _CLN    equ 'C' ; 67
-    _DUP    equ 'D' ; 68
-    _EXCH   equ 'E' ; 69
-    _GET    equ 'G' ; 71
-    _PUT    equ 'P' ; 80
-    _SNCH   equ 'S' ; 83
-
-    SYS_WRITE equ 1
-    STDOUT    equ 1
-    NUL       equ 0
+    SUM         equ '+' ; 43
+    MULTIPLY    equ '*' ; 42
+    NEGATION    equ '-' ; 61
+    ZERO        equ '0' ; 48
+    NINE        equ '9' ; 57
+    NUMBER      equ 'n' ; 110
+    BRANCH      equ 'B' ; 66
+    CLEAN       equ 'C' ; 67
+    DUPLICATE   equ 'D' ; 68
+    EXCHANGE    equ 'E' ; 69
+    GET         equ 'G' ; 71
+    PUT         equ 'P' ; 80
+    SYNCHRONIZE equ 'S' ; 83
+    NUL         equ 0
+    
+    ;SYS_WRITE   equ 1
+    ;STDOUT      equ 1
+    
+section .bss
+    locks       resb  N*N
 
 section .text
-    global euron
-    extern get_value, put_value
+    global  euron
+    extern  get_value, put_value
 
 euron:          ; "n(rdi), prog(rsi)"
     push    rbp
     mov     rbp, rsp
+    jmp     sequence
 
-.loop:
+.return:
+    pop     rax
+    mov     rsp, rbp
+    pop     rbp
+    ret
+
+sequence:
+    cmp     byte [rsi], NUL
+    jz      euron.return
     ;mov     r8, rdi
     ;mov     r9, rsi
-    ;mov     rsi, N
-    cmp     byte [rsi], NUL
-    jz      .exit
     ;mov     rax, SYS_WRITE
     ;mov     rdi, STDOUT
     ;mov     rdx, 1
     ;syscall
     ;mov     rdi, r8
     ;mov     rsi, r9
-
-    mov     r8, _ZERO
-.try_digit:
-    cmp     r8, _NINE
-    jg      .operations
-
-    cmp     byte [rsi], r8b
-    je      .digit
-    inc     r8
-    jmp     .try_digit
-
-.operations:
-    cmp     byte [rsi], _SUM
-    je      .sum
-
-    cmp     byte [rsi], _MUL
-    je      .muliply
-
-    cmp     byte [rsi], _NEG
-    je      .negation
-
-    cmp     byte [rsi], _NUM
-    je      .number
-
-    cmp     byte [rsi], _BRCH
-    je      .branch
-
-    cmp     byte [rsi], _CLN
-    je      .clean
-
-    cmp     byte [rsi], _DUP
-    je      .duplicate
-
-    cmp     byte [rsi], _EXCH
-    je      .exchange
-
-    cmp     byte [rsi], _GET
-    je      .get
-
-    cmp     byte [rsi], _PUT
-    je      .put
-
-    cmp     byte [rsi], _SNCH
-    je      .synchronize
+    mov     rcx, ZERO
+    jmp     try_digit
 
 .inc:
     inc     rsi
-    jmp     .loop
+    jmp     sequence
+
+try_digit:
+    cmp     rcx, NINE
+    jg      operations
+
+    cmp     byte [rsi], cl
+    je      operations.digit
+    inc     rcx
+    jmp     try_digit
+
+operations:
+    cmp     byte [rsi], SUM
+    je      .sum
+
+    cmp     byte [rsi], MULTIPLY
+    je      .muliply
+
+    cmp     byte [rsi], NEGATION
+    je      .negation
+
+    cmp     byte [rsi], NUMBER
+    je      .number
+
+    cmp     byte [rsi], BRANCH
+    je      .branch
+
+    cmp     byte [rsi], CLEAN
+    je      .clean
+
+    cmp     byte [rsi], DUPLICATE
+    je      .duplicate
+
+    cmp     byte [rsi], EXCHANGE
+    je      .exchange
+
+    cmp     byte [rsi], GET
+    je      .get
+
+    cmp     byte [rsi], PUT
+    je      .put
+
+    cmp     byte [rsi], SYNCHRONIZE
+    je      .synchronize
 
 .sum:
-    pop     r8
-    pop     r9
-    add     r8, r9
-    push    r8
-    jmp     .inc
+    pop     rax
+    pop     rdx
+    add     rax, rdx
+    push    rax
+    jmp     sequence.inc
 
 .muliply:
-    pop     r8
-    pop     r9
-    imul    r8, r9
-    push    r8
-    jmp     .inc
+    pop     rax
+    pop     rdx
+    mul     rdx
+    push    rax
+    jmp     sequence.inc
 
 .negation:
-    pop     rcx
-    neg     rcx
-    push    rcx
-    jmp     .inc
+    pop     rax
+    neg     rax
+    push    rax
+    jmp     sequence.inc
 
 .digit:
-    sub     r8, _ZERO
-    push    r8
-    jmp     .inc
+    sub     rcx, ZERO
+    push    rcx
+    jmp     sequence.inc
 
 .number:
     push    rdi
-    jmp     .inc
+    jmp     sequence.inc
 
 .branch:
-    pop     r8
-    cmp     QWORD [rsp], 0
-    je      .inc
-    add     rsi, r8
+    pop     rcx
+    cmp     QWORD [rsp], NUL
+    je      sequence.inc
+    add     rsi, rcx
     ;test    r8, r8
     ;jg      .loop
-    jmp     .inc
+    jmp     sequence.inc
 
 .clean:
-    pop     r8
-    jmp     .inc
+    pop     rax
+    jmp     sequence.inc
 
 .duplicate:
-    mov     r8, [rsp]
-    push    r8
-    jmp     .inc
+    mov     rax, [rsp]
+    push    rax
+    jmp     sequence.inc
 
 .exchange:
-    pop     r8
-    pop     r9
-    push    r8
-    push    r9
-    jmp     .inc
+    pop     rax
+    pop     rdx
+    push    rax
+    push    rdx
+    jmp     sequence.inc
 
 .get:
     push    rsi
@@ -147,25 +157,30 @@ euron:          ; "n(rdi), prog(rsi)"
     pop     rdi
     pop     rsi
     push    rax
-    jmp     .inc
-
+    jmp     sequence.inc
 
 .put:
-    pop     r8
+    pop     rax
     push    rsi
     push    rdi
-    mov     rsi, r8
+    mov     rsi, rax
     call    put_value
     pop     rdi
     pop     rsi
-    jmp     .inc
+    jmp     sequence.inc
 
 .synchronize:
-    jmp     .inc
+    pop     rcx
+    ; TODO
+    mov     rax, N      ; ROW SIZE (N COLUMNS PER ROW).
+    mov     rbx, 35     ; DESIRED ROW (35).
+    mul     rbx         ; EAX * EBX = 8960.
 
-.exit:
-    pop     rax
-    mov     rsp, rbp
-    pop     rbp
+    mov     rdx, locks  ; POINT TO ARRAY.
+    add     rdx, rax    ; POINT TO DESIRED ROW (35).
+    add     rdx, 20     ; POINT TO DESIRED COLUMN (20).
+    ; This is for type byte,
+    ; for bigger types it will be necessary
+    ; to multiply by the size (by 2, by 4, etc).
 
-    ret
+    jmp     sequence.inc
