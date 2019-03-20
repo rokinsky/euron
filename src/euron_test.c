21867a9f2d109fe1542f9693bac7a265849b5d9b
rokinsky <ankezy@gmail.com>
2019-03-20 19:57:45 +0100

    simple tests

--- src/euron_test.c
+++ src/euron_test.c
@@ -3,29 +3,46 @@
 #include <assert.h>
 #include "euron.h"
 #include <pthread.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
+#include <memory.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <limits.h>
 
 #define N 2
-#define COMPUTE "01234n+P56789E-+D+*G*1n-+S2ED+E1-+75+-BC"
+#define N_TESTS 2
 
-int64_t *addr;
+typedef struct euron_args {
+  uint64_t id;
+  char* prog;
+} euron_args;
+
+typedef struct Test {
+  uint64_t id;
+  char* prog;
+  uint64_t expected;
+} Test;
+
+const Test tests[N_TESTS][N] = {
+  {
+    {0, "4n+1n-+S", 5},
+    {1, "4n+1n-+S", 4}
+  },
+  {
+    {0, "01234n+P56789E-+D+*G*1n-+S2ED+E1-+75+-BC", 112},
+    {1, "01234n+P56789E-+D+*G*1n-+S2ED+E1-+75+-BC", 56}
+  }
+};
 
 uint64_t get_value(uint64_t n) {
   assert(n < N);
   return n + 1;
-}
-
-void* thread_function(void* arg) {
-  fprintf(stderr, "[thread] pid is %d\n", (int) getpid());
-  int64_t thread_var = *addr;
-
-  fprintf(stderr, "[thread] var: %" PRId64 "\n", thread_var);
-  fprintf(stderr, "[thread] var addr: %p\n", &thread_var);
-
-  while (1);
-  return NULL;
 }
 
 void put_value(uint64_t n, uint64_t v) {
@@ -33,32 +50,53 @@ void put_value(uint64_t n, uint64_t v) {
   assert(v == n + 4);
 }
 
+void* thread_function(void* data) {
+  euron_args args = *((euron_args* ) data);
+  free(data);
+
+  fprintf(stderr, "[euron] id: %" PRIu64 ", prog: %s\n", args.id, args.prog);
+
+  pthread_exit((void *) euron(args.id, args.prog));
+}
+
 int main() {
+  pthread_t thread[N];
+  void *ret;
 
-  //fprintf(stderr, "[main] pid is %d\n", (int) getpid());
-  //int64_t main_var = 42;
-  //addr = &main_var;
-  //fprintf(stderr, "[main] addr: %p\n", addr);
+  fprintf(stderr, "[main] pid is %d\n\n", (int) getpid());
 
-  //pthread_t thread;
-  //pthread_create(&thread, NULL, &thread_function, NULL);
-
-
-  // branch tests
+  // branch tests, one thread
   assert(euron(N, "21-+6-B") == 0);
   assert(euron(N, "15B00002") == 1);
   assert(euron(N, "12ED+E1-+75+-BC") == 4);
   assert(euron(0, "01234n+P56789E-+D+*G*1n-+C2*2ED+E1-+75+-BC") == 112);
   assert(euron(1, "01234n+P56789E-+D+*G*1n-+C7-7-++2ED+E1-+75+-BC") == 56);
 
-  // synchronize tests
-  assert(euron(0, "4n+1n-+S") == 5);
-  assert(euron(1, "4n+1n-+S") == 4);
+  // synchronize tests, two threads
+  for (int j = 0; j < N_TESTS; j++) {
+    fprintf(stderr, "[main] test N %d\n", j);
+    for (int i = 0; i < N; i++) {
+      euron_args *arg = malloc(sizeof(euron_args));
+      arg->id = tests[j][i].id;
+      arg->prog = tests[j][i].prog;
 
-  // 01234n+P56789E-+D+*G*1n-+2*2ED+E1-+75+-BED+E1-+75+-BC
+      if (pthread_create(&(thread[i]), NULL, thread_function, arg) != 0)
+        fprintf(stderr, "pthread_create\n");
+    }
 
-  //fprintf(stderr, "[main] len prog: %lu, is it true? %d\n",
-  //    len, len == strlen(COMPUTE));
-  //while (1);
+    for (int i = 0; i < N; i++) {
+      if (pthread_join(thread[i], &ret) != 0) {
+        fprintf(stderr, "pthread_join\n");
+        exit(1);
+      }
+      fprintf(stderr, "[main] result euron %d is %" PRIu64 "\n",
+          i, (uint64_t) ret);
+      fprintf(stderr, "[main] ULLONG_MAX - res = %" PRIu64 "\n",
+          (uint64_t)ULLONG_MAX - (uint64_t) ret);
+      assert((uint64_t) ret == tests[j][i].expected);
+    }
+    fprintf(stderr, "\n");
+  }
+
   return 0;
 }
