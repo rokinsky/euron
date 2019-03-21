e4c32b2aee161a42cf9522c959dc1f90500c1346
rokinsky <ankezy@gmail.com>
2019-03-21 19:27:12 +0100

    comments and style

--- src/euron.asm
+++ src/euron.asm
@@ -1,16 +1,36 @@
-%define lookup(tab, i) ((tab) + (SCALE) * (i))
+global euron
+extern get_value, put_value
 
+; &arr[i]
+%define lookup(arr, i) ((arr) + (SCALE) * (i))
+
+; ABI: until euron works,
+; $rdi stores uint64_t n, $rsi - const char* prog,
+; so save and revert are used when calling external function.
 %macro save 0
-    push    rdi                          ; saving uint64_t n
-    push    rsi                          ; saving char* prog
+    push    rdi
+    push    rsi
 %endmacro
 
 %macro revert 0
-    pop     rsi                          ; reverting char* prog
-    pop     rdi                          ; reverting uint64_t n
+    pop     rsi
+    pop     rdi
 %endmacro
 
-%macro in_order 1                        ; dq 0, ..., (%1)-1
+; ABI: prologue and epilogue are used to save stack.
+%macro prologue 0
+    push    rbp
+    mov     rbp, rsp
+%endmacro
+
+%macro epilogue 0
+    mov     rsp, rbp
+    pop     rbp
+%endmacro
+
+; It's used only to initialization spinlocks,
+; for in_order N result is almost dq 0, ..., N - 1.
+%macro sequence 1
 %assign i 0
 %rep    %1
     dq i
@@ -34,44 +54,42 @@ PUT         equ 'P'
 SYNCHRONIZE equ 'S'
 NUL         equ 0                        ; '\0' or 0
 SCALE       equ 8                        ; 8 bytes (64 bit)
-CLOSED      equ -1                       ; for spinlocks'
+CLOSED      equ -1                       ; max N
 
 section .data
+; Spinlock per euron, where i := owner, j := euron to sync.
+; locks[i] has 3 states:
+; 1. i      - opened only for i (to put in values[i] stack's top),
+; 2. j      - opened only for j (to get values[i]),
+; 3. CLOSED - acquired by someone (i or j).
 align 8
-locks:      in_order N                   ; spinlocks' initialization
+locks:      sequence N                   ; 1st state initially.
 
 section .bss
+; Protected data, values[i] holds i's value to exchange with j.
 values:     resq N
 
 section .text
-    global  euron
-    extern  get_value, put_value
-
 ; uint64_t euron(uint64_t n, char const *prog);
-euron:                                   ; invariant: n in $rdi, *prog in $rsi
-    push    rbp
-    mov     rbp, rsp
-    jmp     sequence
+euron:
+    prologue
+    jmp     execute
 
 .return:
-    pop     rax
-    mov     rsp, rbp
-    pop     rbp
+    pop     rax                          ; Calculation result.
+    epilogue
     ret
 
-align 8
-sequence:
+execute:                                 ; INVARIANT! $rdi := n, $rsi := *prog
     cmp     byte [rsi], NUL
     jz      euron.return
     mov     rcx, ZERO
     jmp     try_digit
 
-align 8
-.inc:
+.next:
     inc     rsi
-    jmp     sequence
+    jmp     execute
 
-align 8
 try_digit:
     cmp     rcx, NINE
     jg      operations
@@ -81,7 +99,6 @@ try_digit:
     inc     rcx
     jmp     try_digit
 
-align 8
 operations:
     cmp     byte [rsi], SUM
     je      .sum
@@ -121,103 +138,108 @@ operations:
     pop     rdx
     add     rax, rdx
     push    rax
-    jmp     sequence.inc
+    jmp     execute.next
 
 .muliply:
     pop     rax
     pop     rdx
     mul     rdx
     push    rax
-    jmp     sequence.inc
+    jmp     execute.next
 
 .negation:
     pop     rax
     neg     rax
     push    rax
-    jmp     sequence.inc
+    jmp     execute.next
 
 .digit:
     sub     rcx, ZERO
     push    rcx
-    jmp     sequence.inc
+    jmp     execute.next
 
 .number:
     push    rdi
-    jmp     sequence.inc
+    jmp     execute.next
 
 .branch:
-    pop     rcx
+    pop     rax
     cmp     QWORD [rsp], NUL
-    je      sequence.inc
-    add     rsi, rcx
-    jmp     sequence.inc
+    je      execute.next
+    add     rsi, rax
+    jmp     execute.next
 
 .clean:
     pop     rax
-    jmp     sequence.inc
+    jmp     execute.next
 
 .duplicate:
     mov     rax, [rsp]
     push    rax
-    jmp     sequence.inc
+    jmp     execute.next
 
 .exchange:
     pop     rax
     pop     rdx
     push    rax
     push    rdx
-    jmp     sequence.inc
+    jmp     execute.next
 
 .get:
     save
     call    get_value                    ; $rax := get_value(n)
     revert
     push    rax
-    jmp     sequence.inc
+    jmp     execute.next
 
 .put:
-    pop     rax
+    pop     rdx
     save
-    mov     rsi, rax                     ; w := pop()
+    mov     rsi, rdx                     ; w := pop()
     call    put_value                    ; put_value(n, w)
     revert
-    jmp     sequence.inc
+    jmp     execute.next
 
-.synchronize:                            ; $rdi := i
-    mov     rdx, rsi                     ; saving char* prog
-
-    mov     rsi, rdi                     ; $rsi := $rdi
+; REMINDER: i := n (owner, $rdi), j := euron to sync.
+.synchronize:
+    ; non-critical section
+    mov     rdx, rsi                     ; saving *prog
+    pop     rcx                          ; holds j for 1st post-protocol
+    ; pre-protocol
+    mov     rsi, rdi                     ; $rsi := i
     call    acquire                      ; acquire(i, i)
-
-    pop     rsi                          ; $rsi := j
+    ; critical section
     pop     qword [lookup(values, rdi)]  ; values[i] := pop()
-
+    ; post-protocol
+    mov     rsi, rcx                     ; $rsi := j
     call    release                      ; release(i, j)
 
+    ; pre-protocol
     xchg    rsi, rdi                     ; $rdi := j, $rsi := i
     call    acquire                      ; acquire(j, i)
-
+    ; critical section
     push    qword [lookup(values, rdi)]  ; push(values[j])
-
-    push    rsi                          ; saving i (uint64_t n)
-    mov     rsi, rdi                     ; $rdi := j, $rsi := j
+    ; post-protocol
+    push    rsi                          ; saving i / n
+    mov     rsi, rdi                     ; $rsi := j
     call    release                      ; release(j, j)
-
-    pop     rdi                          ; reverting i (uint64_t n)
-    mov     rsi, rdx                     ; reverting char* prog
-
-    jmp     sequence.inc
+    ; non-critical section
+    pop     rdi                          ; reverting i / n
+    mov     rsi, rdx                     ; reverting *prog
+    jmp     execute.next
 
 ; void acquire(uint64_t spinlock, uint64_t expected);
 acquire:
-    lea     r9, [lookup(locks, rdi)]     ; address of locks[spinlock]
-    mov     r8, CLOSED                   ; desired
+    lea     r8, [lookup(locks, rdi)]     ; address of locks[spinlock]
+    mov     r9, CLOSED                   ; desired
+
 align 8
 .loop:
     mov     rax, rsi                     ; expected
     lock \
-    cmpxchg [r9], r8
+    cmpxchg [r8], r9
     jne     .loop
+
     ret
 
 ; void release(uint64_t spinlock, uint64_t desired);
